bb6dd9ad76a2708e4a0d3b6e67a232c5d47d46a6
Chuck Scott <connaryscott@gmail.com>
2011-07-18 12:44:41 -0700

    added rundeck plugin and its template  config for  notifier

---
+++ chef-repo/cookbooks/jenkins/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:jenkins][:plugins][:rundeck][:version] = 2.5
+default[:jenkins][:plugins][:rundeck][:url] = http://192.168.63.132:4440
+default[:jenkins][:plugins][:rundeck][:user] = admin
+default[:jenkins][:plugins][:rundeck][:password] = admin
--- chef-repo/cookbooks/jenkins/recipes/default.rb
+++ chef-repo/cookbooks/jenkins/recipes/default.rb
@@ -13,6 +13,12 @@
 # I'd rather build up a Chef cookbook than strip it down, so here's
 # a good starting point.
 
+rundeckPluginVersion="#{node[:jenkins][:plugins][:rundeck][:version]}"
+rundeckUser="#{node[:jenkins][:plugins][:rundeck][:user]}"
+rundeckPassword="#{node[:jenkins][:plugins][:rundeck][:password]}"
+rundeckUrl="#{node[:jenkins][:plugins][:rundeck][:url]}"
+
+
 include_recipe "apt"
 include_recipe "java"
 
@@ -28,4 +34,26 @@ package "jenkins"
 service "jenkins" do
   supports [:stop, :start, :restart]
   action [:start, :enable]
+end
+
+
+cookbook_file "/var/lib/jenkins/plugins/rundeck.hpi" do
+  source "var/lib/jenkins/plugins/rundeck-#{rundeckPluginVersion}.hpi"
+  mode 0644
+  owner "jenkins"
+  group "nogroup"
+  notifies :restart, resources(:service => "jenkins")
+end
+
+template "/var/lib/jenkins/org.jenkinsci.plugins.rundeck.RundeckNotifier.xml" do
+  source "var/lib/jenkins/org.jenkinsci.plugins.rundeck.RundeckNotifier.xml.erb"
+  mode 0644
+  owner "jenkins"
+  group "nogroup"
+  variables({
+     :url => rundeckUrl,
+     :login => rundeckUser,
+     :password => rundeckPassword
+  })
+  notifies :restart, resources(:service => "jenkins")
 end
