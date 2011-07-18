815d074ba1a5519bbadd61e8fe348fbe701bab19
Chuck Scott <connaryscott@gmail.com>
2011-07-18 11:03:12 -0700

    formatting

--- chef-repo/cookbooks/jenkins/metadata.rb
+++ chef-repo/cookbooks/jenkins/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "connaryscott@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-depends "apt"
-depends "java"
+depends          "apt"
+depends          "java"
 version          "0.0.1"
